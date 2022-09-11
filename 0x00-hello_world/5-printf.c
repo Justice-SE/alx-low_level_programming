01c785fc5cb64072ebda4150725af2b13c5e9d0e
Justice <aguochajustice5577@gmail.com>
2022-09-11 12:57:05 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - a room where programs work
+ * main - like a room where programs work
  * printf: to output a text
  * Return: always 0 (that is return nothing)
  */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piecie of art,\n");
