4dad4509a78b344a92073c709299bdf721b97bac
Justice <aguochajustice5577@gmail.com>
2022-09-11 13:59:51 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - like a room where programs work
- * printf: to output a text
+ * printf: to output a text, expression
  * Return: always 0 (that is return nothing)
  */
 
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piecie of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
